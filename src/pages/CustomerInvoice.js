ffb4daee72ae5183b023c741120da7357fd51c65
itay <itaywzn@gmail.com>
2021-10-24 01:52:38 +0300

    CustomerInvoice.js fixed layout

--- src/pages/CustomerInvoice.js
+++ src/pages/CustomerInvoice.js
@@ -1,72 +1,81 @@
-import {Box, TableFooter, Typography} from "@mui/material";
-import TableContainer from "@mui/material/TableContainer";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
-import TableBody from "@mui/material/TableBody";
-import React from "react";
+import React, {useState} from "react";
 import {Redirect, useParams} from "react-router-dom";
+import {Box, Typography} from "@mui/material";
+import {makeStyles} from "@mui/styles";
 
-function generateUuid() {
-    // taken from stackoverflow
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
+
+function generateInvoiceID() {
+    return Math.floor(Math.random() * 10000)
 }
+
+const useStyles = makeStyles({
+    row:{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 200px',
+    },
+    cell:{
+        textAlign:"left",
+        minWidth:'100px',
+    },
+    priceCell:{
+        paddingLeft:'16px',
+        backgroundColor:"#dddddd"
+    },
+    container:{
+        height:'330px',
+        display: 'grid',
+        gridTemplateRows: ({packagesLen})=>`50px repeat(${packagesLen},50px) auto`,
+    }
+})
 
 export function CustomerInvoice({customers, invoices}) {
     const {customerId} = useParams();
-    if (!customerId) return <Redirect to={'/customers'} />
     const invoice = invoices[customerId];
+    const classes = useStyles({packagesLen: invoice?.packages?.length});
+    const [invoiceId, setInvoiceID] = useState(generateInvoiceID);
+    if (!customerId) return <Redirect to={'/customers'}/>
     if (!invoice?.packages?.length) {
-        const customer = customers.find(customer=> customer.id == customerId);
+        const customer = customers.find(customer => customer.id == customerId);
         return <Typography variant={'h3'}>{customer?.name} has no packages</Typography>
     }
-    return <Box m={10}>
-        <Box display={'flex'} justifyContent={'space-around'}>
-            <Box display={'flex'} flexDirection={'column'}>
+    return <Box  m={10} mb={0}>
+        <Box display={'flex'} m={4} mb={6} justifyContent={'space-between'} alignItems={"flex-start"} >
+            <Box display={'flex'} textAlign={"left"} flexDirection={'column'}>
                 {new Date().toLocaleDateString()}
                 <br/>
                 {invoice.customerName}
             </Box>
-            <Box flexDirection={'column'} display={"flex"} >
+            <Box textAlign={"left"} flexDirection={'column'} display={"flex"}>
                 <Typography variant={'h5'}>Invoice</Typography>
-                <Typography variant={'text'}>{`${generateUuid()}`}</Typography>
+                <Typography variant={'text'}>{`No.${invoiceId}`}</Typography>
             </Box>
         </Box>
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: 650}} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Weight</TableCell>
-                        <TableCell>Price</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {Object.values(invoice.packages).map((row) => {
-console.log(row)
-                        return (
-                            <TableRow
-                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                            >
-                                <TableCell>{row.id}</TableCell>
-                                <TableCell>{row.weight}</TableCell>
-                                <TableCell>{row.price}</TableCell>
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-                <TableFooter>
-                    <TableCell><Typography variant='h6'>Total</Typography></TableCell>
-                    <TableCell>{invoice.totalWeight}</TableCell>
-                    <TableCell>{invoice.totalPrice}</TableCell>
-                </TableFooter>
-            </Table>
-        </TableContainer>
-        <Box display={'flex'} alignItems={'center'} flexDirection={'column'}>
+
+        <Box className={classes.container} >
+
+            <Box height={'50px'} className={classes.row} >
+                <Box  className={classes.cell}><Typography variant='h6'>ID</Typography></Box>
+                <Box className={classes.cell}><Typography variant='h6'>Weight</Typography></Box>
+                <Box className={`${classes.cell} ${classes.priceCell}`}><Typography variant='h6'>Price</Typography></Box>
+            </Box>
+                {Object.values(invoice.packages).map((row) => {
+                    console.log(row)
+                    return (
+                        <Box className={classes.row}
+                        >
+                            <Box className={classes.cell}>{row.id}</Box>
+                            <Box className={classes.cell}>{row.weight}</Box>
+                            <Box className={`${classes.cell} ${classes.priceCell}`}>{row.price}$</Box>
+                        </Box>
+                    )
+                })}
+            <Box className={classes.row}>
+                <Box  className={classes.cell}></Box>
+                <Box display={"flex"} alignItems={"flex-end"} className={classes.cell} >{invoice.totalWeight}kg</Box>
+                <Box display={"flex"} alignItems={"flex-end"}  className={`${classes.cell} ${classes.priceCell}`} ><Typography variant='h5'>Total: {invoice.totalPrice}$</Typography></Box>
+            </Box>
+        </Box>
+        <Box display={'flex'} mt={6} alignItems={'center'} flexDirection={'column'}>
             <Typography>You received {invoice.packages.length} packages</Typography>
             <Typography>Thank you for using our services</Typography>
         </Box>
